e092e7455b54443875c025ca7e5ee303ebe19166
rrsartpl <rrsartpl@gmail.com>
2023-01-06 00:50:17 +0000

    saa

--- client/script.js
+++ client/script.js
@@ -85,8 +85,10 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
+    
+    const response = await fetch('http://localhost:5000', {
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    // const response = await fetch('https://codex-im0y.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
